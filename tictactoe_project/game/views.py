40d9c266ea473e521481e6b2ef21d3434d288e76
prabesh130 <adhikariprabesh828@gmail.com>
2025-09-28 11:47:50 +0545

    just created the UI of the web app for tictactoe

--- tictactoe_project/game/views.py
+++ tictactoe_project/game/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .agent import TicTacToe2D
+import random
+env=TicTacToe2D()
 
-# Create your views here.
+def board_view(request):
+    context={"baord":env.board.tolist(),"current_player":env.current_player}
+    return render(request,"game/board.html",context)
+def make_move(request,row,col):
+    if env.board[row][col]==0:
+        env.step((row,col))
+        if env.current_player==-1 and not env.check_winner():
+            moves=env.available_actions()
+            action=random.choce(moves)
+            env.step(action)
+    context={"board":env.board.tolist(),"current_player":env.current_player}
+    return render(request,"game/board.html",context)
